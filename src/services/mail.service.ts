fef95a1236154b0ec91261cac186cfd1fa762c15
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-03-23 21:58:29 +0100

    fix

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -6,17 +6,10 @@ class MailService {
   private _transporter: Transporter
 
   constructor() {
-<<<<<<< HEAD
     const smtpConfig = {
       port: 465,
       secure: true,
       host: 'smtp.gmail.com',
-=======
-    this._transporter = nodemailer.createTransport({
-      port: 587,
-      secure: false,
-      host: 'smtp.ethereal.email',
->>>>>>> 6a8b6216cc7d91f8af55c65d85feb47f90a3a0a4
       auth: {
         user: process.env.EMAIL_LOGIN,
         pass: process.env.EMAIL_PASSWORD
